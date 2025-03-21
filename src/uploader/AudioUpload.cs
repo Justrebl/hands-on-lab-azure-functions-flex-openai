34a5002be7af01a1b67406fb89c0105043bd2025
Justrebl <durieljulien@gmail.com>
2025-03-21 23:14:37 +0000

    Adding CloneFile

--- src/uploader/AudioUpload.cs
+++ src/uploader/AudioUpload.cs
@@ -37,5 +37,25 @@ namespace FuncStd
             return new OkObjectResult("Uploaded!");
         }
 
+        [Function(nameof(CloneFile))]
+        public async Task<IActionResult> CloneFile(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", "get")] HttpRequest req,
+            string fileName
+        )
+        {
+            _logger.LogInformation("Processing a new audio file upload request");
+
+            // Get the blob in the container named as fileName
+            var blobClient = _audioContainerClient.GetBlobClient(fileName);
+            var content = await blobClient.OpenReadAsync();
+
+            // Create a new blob and copy the content from the existing blob
+            var newBlobName = $"{Guid.NewGuid()}.wav";
+            var newBlobClient = _audioContainerClient.GetBlobClient(newBlobName);
+            await newBlobClient.UploadAsync(content);
+
+            // Store the file as a blob and return a success response
+            return new OkObjectResult("Copied!");
+        }
     }
 }
