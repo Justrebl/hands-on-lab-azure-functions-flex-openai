1df4310905614d09002a905e296757f297706786
Julien S <durieljulien@gmail.com>
2025-03-23 05:29:28 +0000

    fixed post-up.sh

--- scripts/post-up.sh
+++ scripts/post-up.sh
@@ -12,13 +12,13 @@ for cmd in "${commands[@]}"; do
 done
 
 #Get the function blobs_extension key
-blobs_extension=$(az functionapp keys list -n ${AZURE_PROCESSOR_FUNCTION_APP_NAME} -g ${RESOURCE_GROUP} --query "systemKeys.blobs_extension" -o tsv)
+blobs_extension=$(az functionapp keys list -n ${AZURE_PROCESSOR_FUNCTION_APP_NAME} -g ${AZURE_RESOURCE_GROUP} --query "systemKeys.blobs_extension" -o tsv)
 
 # Build the endpoint URL with the function name and extension key and create the event subscription
 endpointUrl="https://${AZURE_PROCESSOR_FUNCTION_APP_NAME}.azurewebsites.net/runtime/webhooks/blobs?functionName=Host.Functions.AudioBlobUploadStart&code=${blobs_extension}"
 filter="/blobServices/default/containers/${AUDIOS_STORAGE_ACCOUNT_CONTAINER_NAME}"
 
-az eventgrid system-topic event-subscription create -n "audio-files-topic-subscription" -g "${RESOURCE_GROUP}" --system-topic-name "${AUDIOS_EVENTGRID_SYSTEM_TOPIC_NAME}" --endpoint-type "webhook" --endpoint "$endpointUrl" --included-event-types "Microsoft.Storage.BlobCreated" --subject-begins-with "$filter" 
+az eventgrid system-topic event-subscription create -n "audio-files-topic-subscription" -g "${AZURE_RESOURCE_GROUP}" --system-topic-name "${AUDIOS_EVENTGRID_SYSTEM_TOPIC_NAME}" --endpoint-type "webhook" --endpoint "$endpointUrl" --included-event-types "Microsoft.Storage.BlobCreated" --subject-begins-with "$filter" 
 
 echo "Created blob event grid subscription successfully."
 
